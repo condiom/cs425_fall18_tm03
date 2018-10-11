6838a10ca5f8aa3d209bac6045fe3177620711e5
condiom <cdiomi01@cs.ucy.ac.cy>
2018-10-11 22:11:01 +0300

    server template and server thread template

---
+++ src/cs425_fall18_tm03/ServerThread.java
@@ -0,0 +1,19 @@
+package cs425_fall18_tm03;
+
+
+import java.net.*;
+
+/**
+ * This thread is responsible to handle client connection.
+ */
+public class ServerThread extends Thread {
+	Socket socket;
+
+	public ServerThread(Socket socket) {
+		this.socket = socket;
+	}
+
+	public void run() {
+		// TODO
+	}
+}
