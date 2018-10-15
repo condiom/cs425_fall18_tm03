88b3bafa3a7eee289d85a5a26db62fafbf315f84
ekisso01@cs.ucy.ac.cy <ekisso01@cs.ucy.ac.cy>
2018-10-15 14:54:50 +0300

    server thread created

--- src/cs425_fall18_tm03/ServerThread.java
+++ src/cs425_fall18_tm03/ServerThread.java
@@ -1,6 +1,11 @@
 package cs425_fall18_tm03;
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.*;
 
 /**
@@ -15,5 +20,41 @@ public class ServerThread extends Thread {
 
 	public void run() {
 		System.out.println("Server thread started");
+		try {
+			InputStream input = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+			OutputStream output = socket.getOutputStream();
+			PrintWriter writer = new PrintWriter(output, true);
+
+			String requestText;
+			String payload = "";
+			for (int i = 0; i < 10000; i++) {
+				payload += "abcdefghijklmnopqrstuvwyz";
+			}
+			long startTime = System.currentTimeMillis();
+			int counter = 0;
+			do {
+				requestText = reader.readLine();
+				// System.out.println(requestText);
+				if (requestText.equals("Bye"))
+					break;
+				counter++;
+				String requestTextArray[] = requestText.split(" ");
+				String responseText = "WELCOME " + requestTextArray[requestTextArray.length - 1] + payload;
+
+				writer.println(responseText);
+
+			} while (true);
+			long finishTime = System.currentTimeMillis();
+
+			double totalTime = finishTime - startTime;
+			double throughput = counter / totalTime;
+			System.out.println(throughput * 1000);
+			socket.close();
+		} catch (IOException ex) {
+			System.out.println("Server exception: " + ex.getMessage());
+			ex.printStackTrace();
+		}
 	}
 }
